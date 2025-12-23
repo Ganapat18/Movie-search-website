daf1d5acf7624ca1e7285448c6e78abca3e8eca9
Ganapatkumar choudhari <ganpat8660@gmail.com>
2025-12-23 09:28:53 +0530

    Backend deploy config

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,9 @@ app.get("/movies/:name", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running");
 });
+
+
